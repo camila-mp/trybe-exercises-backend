6ff8d9fea402dd5140cb9181912329c2d20c50ba
camila-mp <ca.malvp@gmail.com>
2021-11-15 18:22:00 -0300

    corrige um require no index do hello-msc e adiciona rota /ping no cep-lookup

--- bloco_26/dia26.2/cep-lookup/index.js
+++ bloco_26/dia26.2/cep-lookup/index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.get('/ping', (_req, res) => res.status(200).json({ 'message': 'pong!' }));
+
 app.listen(PORT, () => {
     console.log(`Ouvindo a porta ${PORT}`);
 });
