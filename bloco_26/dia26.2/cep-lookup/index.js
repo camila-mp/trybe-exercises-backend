95359d291ad00889a3dfcb311957ff34b46563b0
camila-mp <ca.malvp@gmail.com>
2021-11-15 18:31:09 -0300

    tira process.env e corrige detalhe em /ping

--- bloco_26/dia26.2/cep-lookup/index.js
+++ bloco_26/dia26.2/cep-lookup/index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.get('/ping', (_req, res) => res.status(200).json({ 'message': 'pong!' }));
+app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong!' }));
 
 app.listen(PORT, () => {
     console.log(`Ouvindo a porta ${PORT}`);
