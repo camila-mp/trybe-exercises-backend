d58dd2b9f5a302e6c28a5e4a9da89a624e099b6b
camila-mp <ca.malvp@gmail.com>
2022-01-14 19:02:40 -0300

    refatora o exemplo

--- bloco_30/dia30.3/exemplo/index.js
+++ bloco_30/dia30.3/exemplo/index.js
@@ -6,18 +6,12 @@ const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000', // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
-  }});
-
-io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id} `);
-
-  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)');
-
-  socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping!`);
-    io.emit('pong', `${socket.id} enviou um ping!`); // essa linha envia um aviso para o cliente que o ping chegou.
-  });
+  }
 });
+
+require('./sockets/ping')(io);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/index.html');
